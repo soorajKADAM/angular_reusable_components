f29acd7c24b6f87b57005a060e4d360871c9c158
soorajKADAM <soorajak102@gmail.com>
2022-05-05 15:58:28 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reusability';
+  users =[{
+    name:'sam',
+    age:25,
+    id:1
+  },
+  {
+    name:'alex',
+    age:26,
+    id:2
+  },
+  {
+    name:'elle',
+    age:24,
+    id:3
+    },
+    {
+      name:'rachel',
+      age:25,
+      id:4
+    }
+]
 }
